9ca11d36d41150a10f0587fdd8e7bea0a0fe99bd
Beau Lyddon <lyddonb@gmail.com>
2017-03-09 13:17:19 -0700

    Add big query integration and docs

---
+++ docs/big_query/wiki/curl_wiki_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -O https://dumps.wikimedia.org/other/pagecounts-raw/2016/2016-01/pagecounts-20160108-110000.gz
--- src/handler/__init__.py
+++ src/handler/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_restful import Api
 
+from big_query import BigQueryLanguages
+from big_query import BigQueryAggLanguages
 from hacker_news import HackerNews
 from highest_seen import HighestSeen
 from natural_langage import NaturalLanguage
@@ -15,3 +17,5 @@ api.add_resource(HighestSeen, '/highest_seen')
 api.add_resource(NaturalLanguage, '/natural_language')
 api.add_resource(HackerNews, '/hacker_news')
 api.add_resource(Rank, '/rank')
+api.add_resource(BigQueryLanguages, '/big_query/languages')
+api.add_resource(BigQueryAggLanguages, '/big_query/languages/aggregated')
